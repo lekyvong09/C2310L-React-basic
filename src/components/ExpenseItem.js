df6c2c651ff75107d92f968b162b98af353e7e00
lekyvong09 <lekyvong09@gmail.com>
2024-04-27 21:07:39 +0700

    2024 Apr 27 -1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
-
+import './ExpenseItem.css';
 
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+    var expenseDate = new Date(2024, 4, 27);
+    var expenseTitle = 'Petrol Gas';
+    var expenseAmount = 2;
+
+    return (
+        <div className="expense-item">
+            <h2>{expenseDate.toLocaleString()}</h2>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price ">$ {expenseAmount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
