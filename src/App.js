cb5a2bb6342c7f04b19538274c65f9ef8116d6d3
lekyvong09 <lekyvong09@gmail.com>
2024-04-27 21:31:20 +0700

    2024 apr 27 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenses = [
+    {id: 1, title: 'Petrol Gas', amount: 5, date: new Date(2024, 4, 27)}, /// expense[0]
+    {id: 2, title: 'Cinema', amount: 5, date: new Date(2024, 4, 30)}, /// expense[1]
+    {id: 3, title: 'Coffee', amount: 5, date: new Date(2024, 4, 20)}, /// expense[2]
+    {id: 4, title: 'Dinner', amount: 20, date: new Date(2024, 4, 10)}, /// expense[3]
+  ];
+
   return (
     <div>
       <h2>Hello World</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem var1={expenses[1].title} var2={expenses[1].amount} var3={expenses[1].date}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    var expenseDate = new Date(2024, 4, 27);
-    var expenseTitle = 'Petrol Gas';
-    var expenseAmount = 2;
+function ExpenseItem(props) { /// properties
+
 
     return (
         <div className="expense-item">
-            <h2>{expenseDate.toLocaleString()}</h2>
+            <h2>{props.var3.toLocaleString()}</h2>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price ">$ {expenseAmount}</div>
+                <h2>{props.var1}</h2>
+                <div className="expense-item__price ">$ {props.var2}</div>
             </div>
         </div>
     );
