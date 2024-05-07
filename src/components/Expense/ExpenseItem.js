5c55aa51f0d05fe60c167a473e24edbc0c35fccd
lekyvong09 <lekyvong09@gmail.com>
2024-05-07 20:25:10 +0700

    2024 May 7 - 2

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) { /// Expense
+function ExpenseItem(props) { /// Expense 
     /// props.expense = {id: 1, title: 'Petrol Gas', amount: 5, date: new Date(2024, 4, 27)}
+
+    var [title, setTitle] = useState(props.expense.title);
+    /// gan gia tri --> setTitle
+    /// lay gia tri --> goi bien title
+    /// gia tri khoi tao --> props.expense.title
+
+
+    const clickHandler = () => {
+        setTitle(title + ' has changed');
+        console.log(title);
+    }
+
     return (
         <Card className='expense-item'>
             {/* <div>{props.expense.date.toISOString()}</div> */}
             <ExpenseDate date={props.expense.date} ></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{props.expense.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>$ {props.expense.amount}</div>
-                <button onClick={() => {console.log('click');}}  >Click me</button>
+                <button onClick={clickHandler}  >Click me</button>
             </div>
         </Card>
     );
