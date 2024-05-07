519c87bab4f558bd528127cf1c665f7b61a1cff0
lekyvong09 <lekyvong09@gmail.com>
2024-05-07 21:40:43 +0700

    2024 May 7 -4

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,29 +1,35 @@
 import { Send } from "@mui/icons-material";
-import { Button, TextField } from "@mui/material";
+import { Button, Container, Stack, TextField } from "@mui/material";
 
 function ExpenseForm() {
     return (
-        <form>
-            <TextField 
-                id="form-title" 
-                label="Title" 
-                variant="outlined" 
-            />
-            <TextField 
-                id="form-amount" 
-                label="Amount" 
-                type="number"
-                variant="outlined" 
-            />
-            <Button 
-                variant="contained" 
-                endIcon={< Send />}
-                type="submit"
-                color="primary"
-            >
-                Send
-            </Button>
-        </form>
+        <Container sx={{width: '50%'}}>
+            <form>
+                <Stack spacing={2} pt={5}>
+                    <TextField 
+                        id="form-title" 
+                        label="Title" 
+                        variant="outlined" 
+                    />
+                    <TextField 
+                        id="form-amount" 
+                        label="Amount" 
+                        type="number"
+                        variant="outlined" 
+                    />
+                </Stack>
+                
+                <Button 
+                    variant="contained" 
+                    endIcon={< Send />}
+                    type="submit"
+                    color="primary"
+                >
+                    Send
+                </Button>
+            </form>
+        </Container>
+        
     );
 }
 
