4709125efec47e3fca512fb744abfc5155af55e0
lekyvong09 <lekyvong09@gmail.com>
2024-05-11 21:33:46 +0700

    2024 May 11

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Expense from './components/Expense/Expense';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const initialExpenses = [
+  {id: 1, title: 'Petrol Gas', amount: 5, date: new Date(2024, 4, 27)}, /// expense[0]
+  {id: 2, title: 'Cinema', amount: 5, date: new Date(2024, 4, 30)}, /// expense[1]
+  {id: 3, title: 'Coffee', amount: 5, date: new Date(2024, 4, 20)}, /// expense[2]
+  {id: 4, title: 'Dinner', amount: 20, date: new Date(2024, 4, 10)}, /// expense[3]
+];
+
 function App() {
 
-  const expenses = [
-    {id: 1, title: 'Petrol Gas', amount: 5, date: new Date(2024, 4, 27)}, /// expense[0]
-    {id: 2, title: 'Cinema', amount: 5, date: new Date(2024, 4, 30)}, /// expense[1]
-    {id: 3, title: 'Coffee', amount: 5, date: new Date(2024, 4, 20)}, /// expense[2]
-    {id: 4, title: 'Dinner', amount: 20, date: new Date(2024, 4, 10)}, /// expense[3]
-  ];
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  const addExpenseHandler = (data) => {
+    // setExpenses([...expenses, data]);
+    setExpenses(previousState => {
+      return [data, ...previousState];
+    });
+  }
 
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpenseHandler={addExpenseHandler}></NewExpense>
       <Expense bien1={expenses} ></Expense>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,17 @@
 import ExpenseForm from "./ExpenseForm";
 
 
-function NewExpense() {
+function NewExpense(props) {
 
     const saveExpense = (data) => {
-        console.log(data);
+        const newData = {
+            ...data,
+            // title: '222',
+            // amount: 222,
+            // date: new Date(efew),
+            id: Math.random()
+        };
+        props.onAddExpenseHandler(newData);
     }
 
     return (
@@ -13,5 +20,6 @@ function NewExpense() {
         </>
     );
 }
+
 
 export default NewExpense;
